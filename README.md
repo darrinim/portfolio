6932938160b20c4afdf22e4e744d2ce392ba79c1
darrinim <darrinj.im@gmail.com>
2019-08-09 10:31:19 -0400

    BASIC LAYOUT/STRUCTURE

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Portfolio
-# Project One
+# DARRIN IM PORTFOLIO PROJECT
+
+##Introduction: The project is aimed to display my work in a clean and elegant fashion. Different mood settings are set to switch between various color schemes based on the mood the user selects.
+
+###Technologies: My portfolio uses primarily HTML, CSS, and vanilla JavaScript. Frameworks will include bootstrap/materialize for hamburger menus and portfolio carousel.
--- scripts.js
+++ scripts.js
@@ -5,15 +5,33 @@ console.log(currentMood);
 const changeMood = (e) => {
   e.preventDefault(); // I think I still need this...
   console.log(e.target.value); // just checking the value of what I'm grabbing
-  let elegantElements = document.querySelectorAll(`ul.${currentMood}`); // this was just initial test to prove it worked. It is currently grabbing all the ul's and their 'currentMood' which is the value the user selects from the dropdown. I will have to adjust most likely to grab specific elements...keep adding new variable names to make it easier
+  let elegantElements = document.querySelectorAll(`.${currentMood}`); // Initial test to prove it worked
   console.log(elegantElements);
-  // I have to loop through all the elements that I grabbed with through the variable elegantElements (FOR THE LOVE OF GOD CHANGE THE DAMN VARIABLE NAMES, SOUNDED GREAT AT FIRST TO PROVE A POINT BUT NOW IT'S CONFUSING) and then just toggling the class name
+  // Have to loop through all the elements that I grabbed with through the variable elegantElements and then just toggling the class name
   for(let i = 0; i < elegantElements.length; i++) {
     elegantElements[i].classList.toggle(currentMood);
     elegantElements[i].classList.toggle(e.target.value)
     console.log(elegantElements[i].classList);
   }
-  currentMood = e.target.value; // Why did I have to put this here? why do you ask? WHY? BECAUSE IT KEPT LOOPING THROUGH AND RECHANGING THINGS
+  currentMood = e.target.value; // Placed this here because it kept repeating
 }
 let selectMood = document.querySelector('#mood');
 selectMood.addEventListener('change', changeMood)
+
+
+
+
+// BELOW IS CLICK EVENT FOR HAMBURGER menu
+
+const hamburger = document.querySelector('.fa-bars')
+const cancel = document.querySelector('.fa-times')
+
+
+hamburger.addEventListener('click', function () {
+  document.querySelector('.mobile-nav').style.right = '0px';
+  console.log(hamburger);
+})
+cancel.addEventListener('click', function () {
+  document.querySelector('.mobile-nav').style.right = '-50%';
+  console.log(cancel);
+})
