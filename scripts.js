ef8daa774c8dda0fa0c013f8a3809f37b3568d9c
darrinim <darrinj.im@gmail.com>
2019-08-10 21:13:05 -0400

    COMPLETED GOOGLE SHEET API IMPLEMENTATION

--- scripts.js
+++ scripts.js
@@ -35,3 +35,104 @@ cancel.addEventListener('click', function () {
   document.querySelector('.mobile-nav').style.right = '-50%';
   console.log(cancel);
 })
+
+//
+// let originalURL = 'https://docs.google.com/spreadsheets/d/10c8Z-7FMbj_htFGaNwAjuOKKMSfKReuVC05ObQqhQqc/edit#gid=0'
+// let id = '10c8Z-7FMbj_htFGaNwAjuOKKMSfKReuVC05ObQqhQqc'
+// let source = `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
+
+// BELOW IS API CONNECTION
+
+// original NEW url https://docs.google.com/spreadsheets/d/1K-A_YU_SC9yevfyvhsw97skP48P8VARCRoJPzav1Tc4/edit#gid=0
+// link that was given on the popup https://docs.google.com/spreadsheets/d/e/2PACX-1vR9PfMdbSCKoGZNZ0eCAekS6dwma-V8PNp_pEoh3nOC7Dn35rMdgtmTzRxRbXF9gcPIyDeacYElUVvQ/pubhtml
+// https://docs.google.com/spreadsheets/d/1K-A_YU_SC9yevfyvhsw97skP48P8VARCRoJPzav1Tc4/edit#gid=0
+// I believe this is the ID '1K-A_YU_SC9yevfyvhsw97skP48P8VARCRoJPzav1Tc4'
+let originalURL = 'https://docs.google.com/spreadsheets/d/1K-A_YU_SC9yevfyvhsw97skP48P8VARCRoJPzav1Tc4/edit#gid=0'
+
+// ID COMES FROM THE URL THAT IS IN THE ADDRESS BAR ONCE THE SHEET HAS BEEN CREATED/SHARED
+// let id =  '15PmioBi2dQEkewpqI7MDkDpvcVF0Trw8vmarAQbwoHk'
+
+let id = '1K-A_YU_SC9yevfyvhsw97skP48P8VARCRoJPzav1Tc4'
+
+
+// BELOW URL IS HOW GOOGLE ALLOWS US TO ACCESS THE SHARED FILE AS JSON
+// let source = `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
+let source = `https://spreadsheets.google.com/feeds/list/${id}/od6/public/values?alt=json`
+
+// https://spreadsheets.google.com/feeds/list/15PmioBi2dQEkewpqI7MDkDpvcVF0Trw8vmarAQbwoHk/1/public/values?alt=json
+
+fetch(source)
+  .then( res => res.json())
+  .then( data => {
+     console.log('this id data.feed.entry', data.feed.entry)
+     let projects = data.feed.entry.map( d => {
+       return {
+          title: d.gsx$title.$t,
+          image: d.gsx$image.$t,
+          description: d.gsx$description.$t
+       }
+     });
+     console.log('this is  projects', projects)
+     createCards(projects)
+})
+
+
+class Card {
+  constructor(obj) {
+    this.title = obj.title
+    this.image = obj.image
+    this.description = obj.description
+  }
+
+  render() {
+    const col = document.createElement('div')
+    col.classList.add('carousel-item');
+
+    const image  = document.createElement('img')
+    image.setAttribute('src', this.image)
+    image.classList.add('w-100');
+
+    const cardTitle = document.createElement('span')
+    cardTitle.classList.add('card-title');
+    cardTitle.innerText = this.title
+
+    // const cardContent = new CardContent(this.description)
+    // console.log('this is cardConten ', cardContent)
+    // col.appendChild(cardTitle)
+    col.appendChild(image)
+   // card.appendChild(cardTitle)
+   //  card.appendChild(cardImage)
+
+    // card.appendChild(cardContent.render())
+    // col.appendChild(card)
+
+    return col
+  }
+}
+
+class CardContent {
+  constructor(desc) {
+    this.desc = desc
+  }
+  render () {
+    console.log('this is desc', this.desc)
+    const cardContent = document.createElement('div')
+    cardContent.classList.add('card-content');
+
+    const paragraph = document.createElement('p')
+    paragraph.innerText = this.desc
+
+    cardContent.appendChild(paragraph)
+    return cardContent
+  }
+}
+
+function createCards(projects){
+  const projectDiv = document.querySelector('.carousel-inner')
+  projects.forEach( obj => {
+    let card = new Card(obj)
+    console.log('this is card', card)
+    projectDiv.appendChild(card.render())
+  })
+    document.querySelector('.carousel-item').classList.add('active');
+}
